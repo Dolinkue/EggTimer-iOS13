fb00173d1e03f780aade3498af780eac824bf7d5
dolinkue <dolinkue_n@me.com>
2022-01-31 20:44:39 -0300

    barra de tiempo y mejoras

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,25 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var eggTimes = ["Soft": 3, "Medium": 5,"Hard":8]
+    var eggTimes = ["Soft": 10, "Medium": 15,"Hard":18]
+    
+    
     var timer = Timer()
+    var totalTime =  0
+    var secondsPassed = 0
     
     
     
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        
+        
         timer.invalidate()
         
         let hardness = sender.currentTitle!
         
+        totalTime = eggTimes[hardness]!
+        barpro.progress = 0.0
+        secondsPassed = 0
+        titulo.text = hardness
+        
+        
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
-            if self.eggTimes[hardness]! > 0 {
-                print ("\(String(describing: self.eggTimes[hardness]!)) seconds")
-                self.eggTimes[hardness]! -= 1
+            
+            if secondsPassed < totalTime {
+            
+                barpro.progress = Float(secondsPassed)/Float(totalTime)
+                
+                secondsPassed += 1
+                
                 } else {
                     Timer.invalidate()
-                    self.titulo.text = "done"
+                    self.titulo.text = "DONE!"
+                    barpro.progress = 1.0
                 }
             
         
@@ -36,6 +53,13 @@ class ViewController: UIViewController {
 }
     @IBOutlet weak var titulo: UILabel!
     
-      
+    @IBOutlet weak var barpro: UIProgressView!
+    
+        
+        
+    
+    
+    
+    
     
 }
